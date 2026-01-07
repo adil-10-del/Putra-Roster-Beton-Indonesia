41be827e3e425b7d26b31ff6c7b290a094b1f907
rostergallery <muhamadadilatu@gmail.com>
2026-01-07 17:56:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-// Future enhancement ready
-console.log("Putra Roster Beton Indonesia - Homepage loaded");
+const slides = document.querySelectorAll(".slide");
+let index = 0;
+
+setInterval(() => {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+}, 5000);
