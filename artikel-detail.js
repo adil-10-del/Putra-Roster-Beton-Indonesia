82e2743b64578f6780d222ca44c6c642133d89e8
rostergallery <muhamadadilatu@gmail.com>
2026-01-08 22:30:44 +0700

    Update artikel-detail.js

--- artikel-detail.js
+++ artikel-detail.js
@@ -4,11 +4,14 @@ const slug = params.get("slug");
 const article = articles.find(a => a.slug === slug);
 
 if (article) {
-  document.title = article.title + " | Putra Roster Beton";
+  const img = randomImage(article.category);
+
+  document.title = article.title + " | Putra Roster Beton Indonesia";
   document.getElementById("articleTitle").innerText = article.title;
   document.getElementById("articleMeta").innerText =
     `${article.date} • ${article.author}`;
-  document.getElementById("articleImage").src = article.image;
+  document.getElementById("articleImage").src = img;
   document.getElementById("articleContent").innerHTML = article.content;
 }
+
 
